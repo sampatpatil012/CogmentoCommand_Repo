6f95ff66afd5e05dae192f28430b30116c1370e4
Vaishali <sampatpatil012@gmail.com>
2025-01-25 17:03:52 +0530

    I Have created the Demo 001 Package

---
+++ src/Demo001/demo001.java
@@ -0,0 +1,10 @@
+package Demo001;
+
+public class demo001 {
+
+	public static void main(String[] args) {
+		System.out.println("Demo 001 ============ ");
+
+	}
+
+}
---
+++ src/Demo22/Demo2121.java
@@ -0,0 +1,10 @@
+package Demo22;
+
+public class Demo2121 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
